d9954eecad4b7bee04e904dc3874068622856e05
Nyankotrain <59917387+neko001robov@users.noreply.github.com>
2020-12-28 03:55:32 +0000

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,3 +1,54 @@
+<div id="top">
+  <div class="home">
+    <a href="/">
+      <img src="/icon.png" width="60" height="60">
+      <font size="5" class="gazoyoko">Home</font>
+    </a>
+  </div>
+  <div class="ma">
+    <div class="profile">
+      <a href="/profile">
+        <font size="5">Profile</font>
+      </a>
+    </div>
+    <div class="list">
+      <a href="/list">
+        <font size="5">記事一覧</font>
+      </a>
+    </div>
+    <div class="Scratch">
+      <a href="//scratch.mit.edu/users/yukku">
+        <font size="5">Scratchのプロフ</font>
+      </a>
+    </div>
+    <div class="comment">
+      <a href="//scratch.mit.edu/studios/28405354/comments">
+        <font size="5">コメントする</font>
+      </a>
+    </div>
+  </div>
+  <div class="menu" id="botn" onclick="menuOpen()">
+    <svg viewBox="0 0 60 60" width="60" height="60">
+      <line x1="10" y1="15" x2="50" y2="15" stroke="#fff" stroke-width="5"/>
+      <line x1="10" y1="30" x2="50" y2="30" stroke="#fff" stroke-width="5"/>
+      <line x1="10" y1="45" x2="50" y2="45" stroke="#fff" stroke-width="5"/>
+    </svg>
+  </div>
+</div>
+<div id="menu"></div>
+<div id="gut">
+  <h2 class="s0">相互リンク</h2>
+<div class="underline"></div>
+<span><img src="https://uploads.scratch.mit.edu/users/avatars/33702239.png" width="30" height="30"><font class="gazoyoko"><a href="//tsumuri3.github.io/homepage/tsumuri">tsumuri3のホームページ</a> by<a href="//scratch.mit.edu/users/tsumuri3">tsumuri3</a></font></span><br>
+<span><img src="https://uploads.scratch.mit.edu/users/avatars/42107291.png" width="30" height="30"><font class="gazoyoko"><a href="//bsahd.github.io">bsahdのページ</a> by<a href="//scratch.mit.edu/users/bsahd">bsahd</a></font></span><br>
+<span><img src="https://uploads.scratch.mit.edu/users/avatars/19147035.png" width="30" height="30"><font class="gazoyoko"><a href="//neko001robov.github.io">nyankotrainのお部屋</a> by<a href="//scratch.mit.edu/users/nyankotrain">nyankotrain</a></font></span>
+  <h2 class="s0">更新情報</h2>
+<div class="underline"></div>
+  <h3>2020/12/25</h3>こういう飛び出るメニューを作りました。
+  <h3>2020/12/24</h3>このサイトを1から作りました。
+</div>
+<div class="main">
+  
 function menuOpen() {
   document.getElementById("botn").onclick = function () {menuClose()}
   document.getElementById("menu").style.setProperty('--menu', '0%');
